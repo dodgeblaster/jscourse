fe12b59a384a6ed9eb9831ee7bf4ca539b211bc3
Gary Jennings <garysjennings@gmail.com>
2019-09-14 18:02:27 -0600

    added ex

---
+++ chapters/Day1/02_Values_Types_andOperators/exercise/app.js
@@ -0,0 +1,8 @@
+let color = prompt('What is your favorite color?')
+let animal = prompt('What is your favorite animal?')
+let house = 'Hufflepuff'
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Your code goes here...
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+document.getElementById('house').innerHTML = house
